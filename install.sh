bad901c19fc35f56703f5e4bd54b3c21143f817b
maximelenormand <maxime.lenormand@inrae.fr>
2026-04-03 07:00:21 +0000

    Update session

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ sudo apt install -y zip libgl1
 
 pip install --upgrade pip
 pip install -r requirements.txt
+
+pip install torch==2.7.1 torchvision==0.22.1 torchaudio==2.7.1 --index-url https://download.pytorch.org/whl/cu118
