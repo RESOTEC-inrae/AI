7972d00334284d5f199c1318e4cd34210637ef8c
jbarrere3 <julienbarrere3@gmail.com>
2026-09-03 15:54:53 +0200

    adjusted summary tables to the new full - balanced split

--- paper/1_summary_stats.R
+++ paper/1_summary_stats.R
@@ -5,6 +5,7 @@ library(sf)
 library(terra)
 library(elevatr)
 library(xtable)
+library(stringr)
 sf::sf_use_s2(FALSE)
 
 # Names of the tables to compile
@@ -196,64 +197,53 @@ print(xtable(out, type = "latex", label = "tablesites",
 # - Table of support per site ----
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-# Initalize the list of outputs
-list.table = vector(mode = "list", length = 2)
-names(list.table) = names
 
-# Loop on names
-for(name in names){
-  
-  # List sites
-  sites <- list.files(path = "data/boundaries")
-  sites <- substr(sites, 1, nchar(sites)-5)
-  n <- length(sites)
-  
-  # Reorder sites
-  sites <- sites[c(1,4,6,7,2,3,5)]
-  
-  # Load data
-  tab <- read.csv("data/images.csv")
-  train <- read.csv(paste0("outputs/train/train_prediction_revue_", name, ".csv"))
-  val <- read.csv(paste0("outputs/val/val_prediction_revue_", name, ".csv"))
-  test <- read.csv(paste0("outputs/test/test_prediction_revue_", name, ".csv"))
-  external <- read.csv(paste0("outputs/external/external_prediction_revue_", name, ".csv")) 
-  
-  
-  # Prepare output
-  res <- as.data.frame(matrix(0,7,5))
-  colnames(res) <- c("Site", "Train", "Validation", "Test", "External")
-  res$Site <- gsub("_", " ", sites)
-  
-  # Loop over site
-  for(k in 1:n){
-    if(k <= 4){
-      res[k,2] <- sum(train$site == sites[k])
-      res[k,3] <- sum(val$site == sites[k])
-      res[k,4] <- sum(test$site == sites[k])
-    }else{
-      res[k,5] <- sum(external$site == sites[k])
-    }
-  }
-  
-  # Compute Total
-  res$Total <- apply(res[,-1], 1, sum)
-  sum(res$Total == table(tab$site)) # 7
-  res <- rbind(res, res[,6])
-  res[8,1] <- "Total"
-  res[8,-1] <- apply(res[1:7,-1], 2, sum)
-  
-  # Add to final list
-  eval(parse(text = paste0("list.table$", name, " = res")))
-  
-}
+# read data 
+assignments = fread("outputs/dataset_assignments.csv") %>%
+  mutate(dataset = str_to_title(dataset))
+images = fread("data/images.csv") %>% 
+  select(image, site) %>%
+  mutate(site = str_to_title(gsub("\\_", "\\ ", site)))
 
-# Compile final table
-table.out = list.table$balanced
-for(i in 1:dim(table.out)[1]) table.out[i, 2:6] = paste0(
-  table.out[i, 2:6], " [", list.table$full[i, 2:6], "]")
-table.out[table.out == "0 [0]"] = "-"
-table.out
+# List sites
+sites = gsub("\\..+", "", gsub("\\_", "\\ ", list.files(path = "data/boundaries")))
+n <- length(sites)
+sites <- sites[c(1,4,6,7,2,3,5)]
 
+
+# Format  with full configuration
+data_full = assignments %>%
+  select(-in_balanced) %>%
+  left_join(images, by = "image") %>%
+  group_by(dataset, site) %>%
+  summarize(n = n()) %>%
+  pivot_wider(names_from = "dataset", values_from = "n") %>%
+  replace(is.na(.), 0) %>%
+  mutate(Total = Training + Test + Validation + External) %>%
+  select(Site = site, Training, Validation, Test, External, Total) %>%
+  rbind(data.frame(
+    Site = "Total", Training = sum(.$Training), Validation = sum(.$Validation), 
+    Test = sum(.$Test), External = sum(.$External), Total = sum(.$Total)))
+
+# Get training support with balanced configuration
+data_balanced = assignments %>%
+  filter(in_balanced) %>%
+  select(-in_balanced) %>%
+  left_join(images, by = "image") %>%
+  group_by(site) %>%
+  summarise(n = n()) %>%
+  rbind(data.frame(site = "Total", n = sum(.$n))) %>%
+  rename(Site = site)
+
+# Assemble in one table
+table.out = data_full %>%
+  left_join(data_balanced, by = "Site") %>%
+  mutate(Training = ifelse(
+    is.na(n), Training, paste0(Training, " [", n, "]")), 
+    Total = ifelse(is.na(n), paste0(Total, " [", Total, "]"), 
+                   paste0(Total, " [", Validation + Test + External + n, "]"))) %>%
+  select(-n) %>%
+  as.data.frame()
 
 # print with tex format
 tex=paste0("\textbf{", table.out$Site, "}", " & ",
@@ -269,36 +259,40 @@ print(data.frame(tex), row.names = FALSE)
 # - Table of support per label ----
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-# Initalize the list of outputs
-list.table = vector(mode = "list", length = 2)
-names(list.table) = names
+# read data 
+assignments = fread("outputs/dataset_assignments.csv") %>%
+  mutate(dataset = str_to_title(dataset))
+annotations = fread("data/annotations.csv")
 
-# Loop on names
-for(name in names){
-  
-  # Read data
-  train <- read.csv(paste0("outputs/train/train_classification_report_", name, ".csv"))
-  val <- read.csv(paste0("outputs/val/val_classification_report_", name, ".csv"))
-  test <- read.csv(paste0("outputs/test/test_classification_report_", name, ".csv"))
-  external <- read.csv(paste0("outputs/external/external_classification_report_", name, ".csv"))
-  
-  # Compile table
-  support <- data.frame(Label = train$labels[1:5], 
-                        Train = train$support[1:5], 
-                        Validation = val$support[1:5],
-                        Test = test$support[1:5],
-                        External = external$support[1:5])
-  support <- support[order(support$Label),]
-  
-  # Add to final list
-  eval(parse(text = paste0("list.table$", name, " = support")))
-  
-}
+# Vector of label names
+labels = colnames(annotations)[2:dim(annotations)[2]]
 
-# Compile final table
-table.out = list.table$balanced
-for(i in 1:dim(table.out)[1]) table.out[i, 2:5] = paste0(
-  table.out[i, 2:5], " [", list.table$full[i, 2:5], "]")
+# Format  with full configuration
+data_full = assignments %>%
+  select(-in_balanced) %>%
+  left_join(annotations, by = "image") %>%
+  pivot_longer(names_to = "Label", values_to = "value", cols = labels) %>%
+  group_by(Label, dataset) %>%
+  summarize(sum = sum(value, na.rm = TRUE)) %>%
+  mutate(sum = as.character(sum)) %>%
+  pivot_wider(names_from = "dataset", values_from = "sum") %>%
+  select(Label, Training, Validation, Test, External)
+
+# Get training support with balanced configuration
+data_balanced = assignments %>%
+  filter(in_balanced) %>%
+  select(-in_balanced) %>%
+  left_join(annotations, by = "image") %>%
+  pivot_longer(names_to = "Label", values_to = "value", cols = labels) %>%
+  group_by(Label) %>%
+  summarise(Training_balanced = sum(value))
+
+# Assemble in one table
+table.out = data_full %>%
+  left_join(data_balanced, by = "Label") %>%
+  mutate(Training = paste0(Training, " [", Training_balanced, "]")) %>%
+  select(-Training_balanced) %>%
+  as.data.frame()
 
 # Print for latex table
 tex=paste0(table.out$Label, " & ",
